a81c24f3ad02ef4f32248ebbf40b536b97e81e7d
Kartik <k.jabreba@gmail.com>
2021-04-08 09:27:26 +0530

    111

--- lib/screens/animationScreen.dart
+++ lib/screens/animationScreen.dart
@@ -8,38 +8,43 @@ class AnimationScreen extends StatefulWidget {
 }
 
 class _AnimationScreenState extends State<AnimationScreen> {
+ 
   @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 3),
-          ()=>Navigator.pushReplacement(context,
-                                        MaterialPageRoute(builder:
-                                                          (context) =>
-                                                          logo()
-                                                         )
-                                       )
-         );
-    // Timer(
-    //   Duration(milliseconds: 2000),
-    //   () => Navigator.of(context).push(PageRouteBuilder(
-    //       pageBuilder: (context, animation, anotherAnimation) {
-    //         return logo();
-    //       },
-    //       transitionDuration: Duration(milliseconds: 2000),
-    //       transitionsBuilder: (context, animation, anotherAnimation, child) {
-    //         // anotherAnimation:CurvedAnimation(curve: Curves.fastLinearToSlowEaseIn, parent: animation);
-    //         animation = CurvedAnimation(
-    //             curve: Curves.fastLinearToSlowEaseIn, parent: animation);
-    //         return SlideTransition(
-    //           position: Tween(begin: Offset(1.0, 0.0), end: Offset(0.0, 0.0))
-    //               .animate(animation),
-    //           child: logo(),
-    //         );
-    //       })),
-    // );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/animationscreen': (context) => AnimationAcreen(),
+        '/afteranimationscreen': (context) => AfterAnimation(),
+      },
+    );
+    
   }
 
-  Widget logo() {
+  Widget aaad() {
+    return Container(
+      color: Colors.white,
+      child: Center(child: Text('cccccccccccccccccccccccccc')),
+    );
+  }
+}
+
+class AnimationAcreen extends StatefulWidget {
+  @override
+  _AnimationAcreenState createState() => _AnimationAcreenState();
+}
+
+class _AnimationAcreenState extends State<AnimationAcreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(milliseconds: 2000),
+      () => Navigator.of(context).popAndPushNamed('/afteranimationscreen');
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       color: Color(0xFF50ABF1),
       child: Center(
@@ -50,7 +55,9 @@ class _AnimationScreenState extends State<AnimationScreen> {
       )),
     );
   }
+}
 
+class AfterAnimation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
